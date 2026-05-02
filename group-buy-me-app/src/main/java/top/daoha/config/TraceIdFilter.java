2d6bec5e920d6aa7621d639f723d8efb8ed219ca
24209 <2420975408@qq.com>
2026-05-02 11:05:27 +0800

    普罗米修斯监控

---
+++ group-buy-me-app/src/main/java/top/daoha/config/TraceIdFilter.java
@@ -0,0 +1,29 @@
+package top.daoha.config;
+
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class TraceIdFilter extends OncePerRequestFilter {
+
+    public static final String TRACE_ID = "trace-id";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try{
+            String traceId= UUID.randomUUID().toString();
+            MDC.put(TRACE_ID,traceId);
+            filterChain.doFilter(request, response);
+        }catch (Exception e){
+            MDC.clear();
+        }
+    }
+}
